6842795c9fce56f7f945b4a14a44e8da5921d4bc
ssaeed97 <mssaeed02@gmail.com>
2024-03-28 12:56:34 -0500

    Basic Setup

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify, render_template, request, redirect, url_for
 import os
 from config import SQLITE_DATABASE_URI
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_smorest import Api, Blueprint, abort
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -9,11 +11,16 @@ app.secret_key = os.urandom(24)
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLITE_DATABASE_URI
 db = SQLAlchemy()
 db.init_app(app)
+migrate = Migrate(app, db)
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     product_name = db.Column(db.String(100), nullable=False)
     brand_name = db.Column(db.String(100), nullable=False)
+    vendor = db.Column(db.String(100), nullable=False,default = "No Vendor Data")
+    price = db.Column(db.Float, default=0.0)
+    in_list = db.Column(db.Boolean, default=False)
+    
 
 with app.app_context():
     db.create_all()
@@ -21,18 +28,24 @@ with app.app_context():
 @app.route('/')
 def index():
     result = db.session.execute(db.select(Product).order_by(Product.id)).scalars()
-    products = [{'id': row.id, 'product_name': row.product_name, 'brand_name': row.brand_name} for row in result]
+    products = [{'id': row.id, 'product_name': row.product_name, 'brand_name': row.brand_name, 'price': row.price if row.price is not None else '', 'vendor': row.vendor} for row in result]
     #products = Product.query.all()
-    for product in products:
-        print(product)
+    return render_template('index.html', products=products)
+
+@app.route('/product/list')
+def product_list():
+    result = db.session.execute(db.select(Product).order_by(Product.id)).scalars()
+    products = [{'id': row.id, 'product_name': row.product_name, 'brand_name': row.brand_name, 'price': row.price if row.price is not None else '', 'vendor': row.vendor} for row in result]
     return render_template('index.html', products=products)
 
 @app.route('/product/add', methods=['GET', 'POST'])
-def add_product():
+def product_add():
     if request.method == 'POST':
         product = Product(
             product_name=request.form["product_name"],
             brand_name=request.form["brand_name"],
+            vendor=request.form["vendor"],
+            price=request.form["price"]
         )
         db.session.add(product)
         db.session.commit()
@@ -40,15 +53,14 @@ def add_product():
     return render_template('product.html')
 
 @app.route("/product/<int:id>")
-def user_detail(id):
-    product = db.get_or_404(Product, id)
-    print(type(product))
-    #print(product.metadata)
-    #return render_template("product_detail.html", product=product)
+def product_detail(id):
+    result = db.session.execute(db.select(Product).where(Product.id == id)).fetchone()
+    product = [{'id': row.id, 'product_name': row.product_name, 'brand_name': row.brand_name, 'price': row.price if row.price is not None else '', 'vendor': row.vendor} for row in result]
     return product
 
+
 @app.route("/product/<int:id>/delete", methods=["GET", "POST"])
-def user_delete(id):
+def product_delete(id):
     product = db.get_or_404(Product, id)
 
     if request.method == "POST":
